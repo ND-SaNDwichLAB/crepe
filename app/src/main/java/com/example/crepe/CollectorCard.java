44f6a88eb159b6edb932e872fcb8eb551527d5ef
Yuwen <yuwenlu@andrew.cmu.edu>
2021-10-26 13:51:42 -0400

    Add Java class to collector card

---
+++ app/src/main/java/com/example/crepe/CollectorCard.java
@@ -0,0 +1,12 @@
+package com.example.crepe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CollectorCard extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collector_card);
+    }
+}
