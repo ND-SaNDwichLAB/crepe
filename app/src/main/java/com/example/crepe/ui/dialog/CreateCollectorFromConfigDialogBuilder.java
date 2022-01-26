fef9280a2a1ccd13bf60316829746a795011ce98
Meng CHEN <mengchen@dhcp-10-7-182-60.dhcp.nd.edu>
2022-01-25 21:27:24 -0500

    new popup; refactor AddFromConfig class

--- app/src/main/java/com/example/crepe/ui/dialog/CreateCollectorFromConfigDialogBuilder.java
+++ app/src/main/java/com/example/crepe/ui/dialog/CreateCollectorFromConfigDialogBuilder.java
@@ -5,43 +5,135 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.crepe.R;
+import com.example.crepe.database.Collector;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 public class CreateCollectorFromConfigDialogBuilder {
 
     private Context c;
     private AlertDialog.Builder dialogBuilder;
+    private String currentScreen;
+
+    //TODO: collectorID, creatorUSerID
+    private Collector collector;
+
+    private View popupView;
+    private Dialog dialog;
+
+
     public CreateCollectorFromConfigDialogBuilder(Context c) {
         this.c = c;
         this.dialogBuilder = new AlertDialog.Builder(c);
-
+        this.currentScreen = "buildDialogFromConfig";
     }
 
-    public Dialog build(){
-        final View popupView = LayoutInflater.from(c).inflate(R.layout.dialog_add_collector_from_config, null);
+    public Dialog build() {
+        updateCurrentView(currentScreen);
         dialogBuilder.setView(popupView);
-        Button popupCancelBtn = (Button) popupView.findViewById(R.id.addCollectorFromConfigDialogCancelButton);
-        Button popupNextBtn = (Button) popupView.findViewById(R.id.addCollectorFromConfigDialogNextButton);
-        Dialog dialog = dialogBuilder.create();
-        popupCancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        popupNextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-                CreateCollectorFromConfigDialogBuilderGraphQuery newPopup = new CreateCollectorFromConfigDialogBuilderGraphQuery(c);
-                Dialog newDialog = newPopup.build();
-                newDialog.show();
-            }
-        });
+        dialog = dialogBuilder.create();
         return dialog;
+    }
+
+    public void updateCurrentView(String currentScreen) {
+        switch(currentScreen){
+            case "buildDialogFromConfig":
+                popupView = LayoutInflater.from(c).inflate(R.layout.dialog_add_collector_from_config, null);
+                dialogBuilder.setView(popupView);
+                //buttons
+                Button popupCancelBtn = (Button) popupView.findViewById(R.id.addCollectorFromConfigDialogCancelButton);
+                Button popupNextBtn = (Button) popupView.findViewById(R.id.addCollectorFromConfigDialogNextButton);
+                //spinners
+                Spinner appDropDown = (Spinner)popupView.findViewById(R.id.appSpinner);
+                Spinner locationDropDown = (Spinner) popupView.findViewById(R.id.locationSpinner);
+                String[] appItems = new String[]{"Uber", "Doordash", "Grubhub"};
+                ArrayAdapter<String> appAdapter = new ArrayAdapter<String>(c.getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, appItems);
+                appAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                appDropDown.setAdapter(appAdapter);
+                String[] locationItems = new String[]{"1", "2", "3"};
+                ArrayAdapter<String> locationAdapter = new ArrayAdapter<String>(c.getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, locationItems);
+                locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                locationDropDown.setAdapter(locationAdapter);
+                //date picker buttons and textview
+                ImageButton startImgBtn = (ImageButton)popupView.findViewById(R.id.startImageButton);
+                ImageButton endImgBtn = (ImageButton)popupView.findViewById(R.id.endImageButton);
+                EditText startDateText = (EditText)popupView.findViewById(R.id.startDateText);
+                EditText endDateText = (EditText)popupView.findViewById(R.id.endDateText);
+                //MaterialDatePicker
+                MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+                builder.setTitleText("SELECT THE START DATE");
+                builder.setSelection(MaterialDatePicker.todayInUtcMilliseconds());
+                final MaterialDatePicker startMaterialDatePicker = builder.build();
+
+                startImgBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startMaterialDatePicker.show(startMaterialDatePicker.getParentFragmentManager(), "start_date_picker");
+
+                    }
+                });
+
+                startImgBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+                Dialog dialog = dialogBuilder.create();
+                popupCancelBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                popupNextBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // update info for collector
+                        //collector.getAppName();
+                        //collector.getTimeCreated();
+                        //collector.getTimeLastEdited();
+
+                        // update currentScreen String value
+                        //currentScreen = "buildDialogFromConfigGraphQuery";
+
+                        // recursively call itself with new currentScreen String value
+                        //updateCurrentView(currentScreen);
+                        dialog.dismiss();
+                        CreateCollectorFromConfigDialogBuilderGraphQuery nextPopup = new CreateCollectorFromConfigDialogBuilderGraphQuery(c);
+                        Dialog newDialog = nextPopup.build();
+                        newDialog.show();
+                    }
+                });
+
+
+
+            case "buildDialogFromConfigGraphQuery":
+                ;
+
+            case "buildDialogFromConfigDataField": ;
+
+
+
+
+            case "buildDialogFromConfigDescription":;
+
+
+
+        }
+
     }
 
 }
