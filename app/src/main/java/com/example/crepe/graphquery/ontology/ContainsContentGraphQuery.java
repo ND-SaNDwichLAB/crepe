db9374f8617d6b2fba95ff761948f7982eed3dab
Yuwen <yuwenlu@andrew.cmu.edu>
2022-07-06 12:20:42 -0400

    Add content for ContainsContentGraphQuery

--- app/src/main/java/com/example/crepe/graphquery/ontology/ContainsContentGraphQuery.java
+++ app/src/main/java/com/example/crepe/graphquery/ontology/ContainsContentGraphQuery.java
@@ -1,6 +1,9 @@
 package com.example.crepe.graphquery.ontology;
 
+import android.util.Log;
 import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
 
 // example: (HAS_TEXT Price)
 public class ContainsContentGraphQuery extends GraphQuery {
@@ -17,7 +20,49 @@ public class ContainsContentGraphQuery extends GraphQuery {
         this.queryString = generateQueryString(this.relation, this.property);
     }
 
+    // the constructor that takes a query string, used after retrieving from the database
+    // TODO yuwen: support recursive operations
+    public ContainsContentGraphQuery(String queryString) {
+        this.queryString = queryString;
+        // take out the parenthesis outside
+        if(queryString.startsWith("(") && queryString.endsWith(")")) {
+            queryString = queryString.substring(1, queryString.length() - 1);
+            String[] parsedResults = queryString.split(" ");
+            if(parsedResults.length == 2) {
+                this.relation = SugiliteRelation.getRelationFromString(parsedResults[0]);
+                this.property = parsedResults[1];
+            } else {
+                Log.d("parse query", "Parse query error, contains more than 2 fields");
+            }
+
+        }
+    }
+
     private String generateQueryString(SugiliteRelation relation, String property) {
         return "(" + relation.getRelationName() + " " + property + ")";
+    }
+
+    public SugiliteRelation getRelation() {
+        return relation;
+    }
+
+    public void setRelation(SugiliteRelation relation) {
+        this.relation = relation;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
     }
 }
