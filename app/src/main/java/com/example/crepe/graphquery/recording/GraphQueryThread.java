64e339617fc095a083dc4bbc74ba7d654ef35f8b
Yuwen <ylu23@nd.edu>
2023-02-13 17:44:20 -0500

    Add files misteriously deleted during merge conflict resolving

---
+++ app/src/main/java/com/example/crepe/graphquery/recording/GraphQueryThread.java
@@ -0,0 +1,35 @@
+package com.example.crepe.graphquery.recording;
+
+import android.os.Looper;
+import android.os.Handler;
+import android.util.Log;
+
+import com.example.crepe.CrepeAccessibilityService;
+import com.example.crepe.graphquery.ontology.UISnapshot;
+
+import java.util.logging.LogRecord;
+
+// This class is responsible for retrieving the data from the UI and sending it to the server
+
+public class GraphQueryThread extends Thread {
+    private static final String TAG = "GraphQueryThread";
+    private CrepeAccessibilityService service;
+    private String graphQuery;
+    public Handler handler;
+    public Looper looper;
+
+
+    public GraphQueryThread() {
+    }
+
+
+    @Override
+    public void run() {
+        Looper.prepare();
+        looper = Looper.myLooper();
+        handler = new Handler();
+        Looper.loop();
+        Log.d(TAG, "GraphQueryThread is running");
+    };
+
+}
