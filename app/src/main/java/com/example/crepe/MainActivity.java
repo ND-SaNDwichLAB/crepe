656baac0acc25c2d02c22640e26b0b7e01b2e969
Yuwen <ylu23@nd.edu>
2023-03-06 17:53:53 -0500

    Adjust theme and update meterial library version

--- app/src/main/java/com/example/crepe/MainActivity.java
+++ app/src/main/java/com/example/crepe/MainActivity.java
@@ -97,10 +97,10 @@ public class MainActivity extends AppCompatActivity {
         }
         try {
             List<Pair<String,String>> testList = new ArrayList<>();
-            testCollector = new Collector("2", "1", "YouTube Music", "com.google.android.apps.youtube.music", "description for a Youtube Music collector", "local", 100, 100,"active");
-            testCollector2 = new Collector("7", "1", "Doordash", "com.app.doordash", "description for a Doordash collector", "what", "https", 139148015, 1491789595,"active");
-            Boolean addResult = dbManager.addOneCollector(testCollector);
-            Boolean addResult2 = dbManager.addOneCollector(testCollector2);
+//            testCollector = new Collector("2", "1", "YouTube Music", "com.google.android.apps.youtube.music", "description for a Youtube Music collector", "local", 100, 100,"active");
+//            testCollector2 = new Collector("7", "1", "Doordash", "com.app.doordash", "description for a Doordash collector", "what", "https", 139148015, 1491789595,"active");
+//            Boolean addResult = dbManager.addOneCollector(testCollector);
+//            Boolean addResult2 = dbManager.addOneCollector(testCollector2);
 
         }
         catch (Exception e) {
