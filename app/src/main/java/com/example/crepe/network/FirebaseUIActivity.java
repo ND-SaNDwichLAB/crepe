395c938d0c97b7c82e84931d118051f2a8634184
Meng Chen <mchen24@nd.edu>
2023-05-22 01:38:43 +0800

    create ui auth

---
+++ app/src/main/java/com/example/crepe/network/FirebaseUIActivity.java
@@ -0,0 +1,43 @@
+package com.example.crepe.network;
+
+import static androidx.activity.result.ActivityResultCallerKt.registerForActivityResult;
+
+import android.content.Intent;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FirebaseUIActivity {
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.PhoneBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build(),
+            new AuthUI.IdpConfig.FacebookBuilder().build(),
+            new AuthUI.IdpConfig.TwitterBuilder().build());
+
+    // Create and launch sign-in intent
+    Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build();
+    signInLauncher.launch(signInIntent);
+}
