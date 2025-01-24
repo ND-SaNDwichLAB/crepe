b0197e833396411370bf005e3e9adbc3a47e5622
Yuwen <ylu23@nd.edu>
2025-01-24 15:32:51 -0500

    Initialize evaluation recorder and pipeline:

---
+++ app/src/main/java/edu/nd/crepe/evalrecorder/EvalRecorder.java
@@ -0,0 +1,14 @@
+package edu.nd.crepe.evalrecorder;
+
+import android.view.accessibility.AccessibilityEvent;
+
+import edu.nd.crepe.graphquery.ontology.UISnapshot;
+
+public class EvalRecorder {
+
+    public EvalRecorder() {}
+
+    public void saveAccessibilityEvent(AccessibilityEvent accessibilityEvent, UISnapshot uiSnapshot, String resultData) {
+
+    }
+}
---
+++ eval/src/main/java/com/example/eval/EvalPipeline.java
@@ -0,0 +1,6 @@
+package com.example.eval;
+
+public class EvalPipeline {
+    // takes in data, runs the pipeline, and returns the result
+    // for each of the saved accessibility event & UISnapshot, run the graph query, get the result, compare and calculate F1 score
+}
